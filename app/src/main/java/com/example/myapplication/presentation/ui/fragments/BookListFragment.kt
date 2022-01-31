e52cbe48754ba2ee252bd5f4ae84da6d211557f3
DESKTOP-VV0J4CP\rickc <hcsantos.cmp@gmail.com>
2022-01-31 11:48:55 -0300

    View models added.

--- app/src/main/java/com/example/myapplication/presentation/ui/fragments/BookListFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/ui/fragments/BookListFragment.kt
@@ -5,19 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.navArgs
+import androidx.fragment.app.viewModels
 import com.example.myapplication.presentation.adapter.BookClickListener
 import com.example.myapplication.presentation.adapter.BookListAdapter
 import com.example.myapplication.databinding.FragmentBookListBinding
+import com.example.myapplication.domain.exception.EmptyBookListException
 import com.example.myapplication.domain.model.Book
+import com.example.myapplication.presentation.viewmodel.BookListViewModel
+import com.example.myapplication.util.ViewState
 
 class BookListFragment : Fragment(), BookClickListener {
-
-    private val args: BookListFragmentArgs by navArgs()
 
     private lateinit var bookListAdapter: BookListAdapter
     private var _binding: FragmentBookListBinding? = null
     private val binding: FragmentBookListBinding get() = _binding!!
+
+    private val viewModel: BookListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,23 +32,55 @@ class BookListFragment : Fragment(), BookClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setBookListData()
+        configureListeners()
+        addObserver()
     }
 
     private fun setBookListData() {
         bookListAdapter = BookListAdapter(this)
         binding.rvBooks.adapter = bookListAdapter
-        bookListAdapter.submitList(
-            Book.getMockList()
-        )
+        viewModel.search()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun configureListeners() {
+        binding.editSearchView.textChangedListener = { input ->
+            viewModel.search(input)
+        }
+    }
+
+    private fun addObserver() {
+        viewModel.bookListViewState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is ViewState.Success -> {
+                    showEmptyListError(false)
+                    bookListAdapter.submitList(
+                        state.data
+                    )
+                }
+                is ViewState.Error -> {
+                    when (state.throwable) {
+                        is EmptyBookListException -> {
+                            bookListAdapter.submitList(listOf())
+                            showEmptyListError(true)
+                        }
+                        else -> Unit
+                    }
+                }
+                else -> Unit
+            }
+        }
+    }
+
+    private fun showEmptyListError(hasError: Boolean) {
+        binding.tvEmptyList.visibility = if (hasError) View.VISIBLE else View.GONE
     }
 
     override fun onBookClickListener(book: Book) {
         BookDetailsBottomSheet.newInstance(book).show(childFragmentManager, "book")
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
